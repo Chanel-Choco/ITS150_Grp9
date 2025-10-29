40bd7029cc202fbfbd04a6e3a27acf7bdbd88497
Chanel-Choco <juliannachanelcn@gmail.com>
2025-10-29 14:30:56 +0800

    Add files to github

---
+++ src/securitysystem/DBConnection.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package securitysystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author Julianna Boado
+ */
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/securitySystem";
+    private static final String USER = "root"; // change if needed
+    private static final String PASSWORD = "Luna28lynn."; // change to your password
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
